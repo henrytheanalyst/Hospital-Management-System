ef35901a7614190cc96d52cca297f25af38f3178
hart <henryhartmwangi@gmail.com>
2026-09-26 14:31:46 +0300

    Worked on the dashboard template layout

--- FrontEnd/st-mary's/src/Components/Dashboard-template.jsx
+++ FrontEnd/st-mary's/src/Components/Dashboard-template.jsx
@@ -1,8 +1,13 @@
+import Header from "./Header";
 function DashboardTemplate(){
     return(
         <section className="dashboard-area">
-            <h2>Overview</h2>
-            <h3>{new Date().toDateString()}-Morning Report</h3>
+            <Header/>
+            <div className="dashboard-content">
+                <h2>Overview</h2>
+                <h3>{new Date().toDateString()}-Morning Report</h3>
+
+            
             <div className="view-dashboard">
                 <div className="view">
                     <h3>Total PAtients</h3>
@@ -52,18 +57,99 @@ function DashboardTemplate(){
                 </div>
             </div>
             <div className="admissions-area">
-                    {/* now here i dont  know if its a table or a form that has the following column
-                        patient_id
-                        name
-                        age
-                        ward
-                        physician
-                        condition
-                        admitted
-                        status
-                    
-                    */}
+                <div className="admissions-header">
+                    <h2>Recent Admissions</h2>
+                    <p>Patients recently admitted to the hospital</p>
                 </div>
+                <div className="table-container">
+                    <table>
+                        <thead>
+                            <th>Patient ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Ward</th>
+                            <th>Physician</th>
+                            <th>Condition</th>
+                            <th>Admitted</th>
+                            <th>Status</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>P-001</td>
+                                <td>John Kamau</td>
+                                <td>45</td>
+                                <td>Cardiology</td>
+                                <td>Dr. Mwangi</td>
+                                <td>Heart Condition</td>
+                                <td>2016-09-01</td>
+                                <td>
+                                    <span className="status addmitted-status">
+                                        Addmitted
+                                    </span>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>P-002</td>
+                                <td>Alex Mwikali</td>
+                                <td>25</td>
+                                <td>Surgery</td>
+                                <td>Dr. Maina</td>
+                                <td>Minor Surgery</td>
+                                <td>2026-09-02</td>
+                                <td>
+                                    <span className="status observation-status">
+                                        Observation
+                                    </span>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>P-003</td>
+                                <td>Jedidah Kwakana</td>
+                                <td>34</td>
+                                <td>Emergency</td>
+                                <td>Dr. Onesmus</td>
+                                <td>Labor pains</td>
+                                <td>2026-09-03</td>
+                                <td>
+                                    <span className="status preop-status">
+                                        Pre-op
+                                    </span>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>P-004</td>
+                                <td>Beth Wambui</td>
+                                <td>33</td>
+                                <td>Cardiology</td>
+                                <td>Dr. Nakamura</td>
+                                <td>Fracture</td>
+                                <td>2026-09-03</td>
+                                <td>
+                                    <span className="status stable-status">
+                                        Stable
+                                    </span>
+                                </td>
+                            </tr>
+                             <tr>
+                                <td>P-001</td>
+                                <td>John Kamau</td>
+                                <td>45</td>
+                                <td>Cardiology</td>
+                                <td>Dr. Mwangi</td>
+                                <td>Heart Condition</td>
+                                <td>08:30 AM</td>
+                                <td>
+                                    <span className="status admitted-status">
+                                        Stable
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                   
+                </div>
+            </div>
         </section>
     );
 }
